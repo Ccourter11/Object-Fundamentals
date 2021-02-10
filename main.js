d690f171da7b94dd50306a6bf60675702194d078
Ccourter11 <courtercalvin@yahoo.com>
2021-02-10 13:01:03 -0600

    to do's

--- main.js
+++ main.js
@@ -4,65 +4,77 @@ console.log("Object Fundamentals Review");
 
 // sample album object
 const tenAlbum = {
-    title: "Ten",
-    artist: "Pearl Jam",
-    released: "8/27/1991",
-    tracks: [
-        {
-            title: "Once",
-            length: 232
-        },
-        {
-            title: "Even Flow",
-            length: 293
-        },
-        {
-            title: "Alive",
-            length: 341
-        },
-        {
-            title: "Why Go",
-            length: 200
-        },
-        {
-            title: "Black",
-            length: 344
-        },
-        {
-            title: "Jeremy",
-            length: 319
-        },
-        {
-            title: "Oceans",
-            length: 162
-        },
-        {
-            title: "Porch",
-            length: 211
-        },
-        {
-            title: "Garden",
-            length: 299
-        },
-        {
-            title: "Deep",
-            length: 258
-        },
-        {
-            title: "Release",
-            length: 543
-        }
-    ]
+  title: "Ten",
+  artist: "Pearl Jam",
+  released: "8/27/1991",
+  tracks: [
+    {
+      title: "Once",
+      length: 232,
+    },
+    {
+      title: "Even Flow",
+      length: 293,
+    },
+    {
+      title: "Alive",
+      length: 341,
+    },
+    {
+      title: "Why Go",
+      length: 200,
+    },
+    {
+      title: "Black",
+      length: 344,
+    },
+    {
+      title: "Jeremy",
+      length: 319,
+    },
+    {
+      title: "Oceans",
+      length: 162,
+    },
+    {
+      title: "Porch",
+      length: 211,
+    },
+    {
+      title: "Garden",
+      length: 299,
+    },
+    {
+      title: "Deep",
+      length: 258,
+    },
+    {
+      title: "Release",
+      length: 543,
+    },
+  ],
 };
 
 // Dot vs. Bracket Notation - accessing properties
 // TODO: USING DOT NOTATION, access the object properties (artist, title, released) and log the following message to the console using string interpolation: "Pearl Jam's Album Ten was released on 8/27/1991"
 
+console.log(
+  `${tenAlbum.artist}'s album ${tenAlbum.title} was released on ${tenAlbum.released}.`
+);
+
 // TODO: Output the same messge to the console using BRACKET NOTATION
+
+console.log(
+  `${tenAlbum["artist"]}'s album ${tenAlbum["title"]} was released on ${tenAlbum["released"]}.`
+);
 
 // Iterating Object keys/values
 // TODO: Iterate the tenAlbum object and log the key/value pairs to the console using a for/in loop
 // example: "title": "Ten"
+
+for (let item in tenAlbum) {
+  console.log(`${item}: ${tenAlbum[item]}`);
+}
 
 // Accessing iterable properties on objects
 // TODO: Iterate the tracks array and output the title of each song and its song length using a for loop of your choosing. The output should look something like this:
