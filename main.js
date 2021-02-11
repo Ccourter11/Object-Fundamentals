a4c0baaaf6de4c43449b1214abd8782a1b9554eb
Ccourter11 <courtercalvin@yahoo.com>
2021-02-10 20:56:58 -0600

    more todo's

--- main.js
+++ main.js
@@ -93,25 +93,28 @@ for (const track of tenAlbum.tracks) {
 // OBJECT.keys() / .values() / .entries()
 // TODO: log the keys of the tenAlbum object using Object.keys()
 console.log(Object.keys(tenAlbum));
+console.log("Object.keys()", Object.keys(tenAlbum));
 
 // TODO: log the values of the tenAlbum object Object.values()
 
 console.log(Object.values(tenAlbum));
+console.log("Object.values()", Object.values(tenAlbum));
 
 // TODO: log the entries of the tenAlbum object Object.entries()
 console.log(Object.entries(tenAlbum));
+console.log("Object.entries()", Object.entries(tenAlbum));
 
 // TODO: iterate the tenAlbum object and log the key/value pairs to the console using each of the above Object methods:
 // .keys()
 // .values()
 // .entries()
 
-const entry = Object.entries(tenAlbum);
-const value = Object.values(tenAlbum);
-const key = Object.keys(tenAlbum);
+// for (let key of Object.keys(tenAlbum)) {
+//   console.log(key, tenAlbum[key]);
+// }
 
-for (const property in tenAlbum) {
-  console.log(`${Object.entries(tenAlbum)}`);
+for (const [key, value] of Object.entries(tenAlbum)) {
+  console.log(`Key: ${key} - Value: ${value}`);
 }
 
 // ! ADD AND COMMIT
@@ -119,9 +122,12 @@ for (const property in tenAlbum) {
 // USING THE DATA FROM .setlists
 import { setlistsFromAPI } from "./setlists.js";
 // TODO: Log 'setlistsFromAPI' to the console
+// const setList = setlistsFromAPI;
+// console.log(setList);
 
 // TODO: USING the 'setlistsFromAPI' data, find the event dated 18-08-2018 and store that event object in a variable named 'event'
-
+// const setList1+ = setlistsFromAPI;
+// console.log(setlist.eventDate);
 // TODO: log the 'event' object to the console
 
 // TODO: USING DOT OR BRACKET NOTATION AND STRING INTERPOLATION, create a string which includes the name of the band, the venue, and the date of the event and store it in a new variable clled "bandVenueDate"
